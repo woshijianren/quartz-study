641b9e0266823ae71a7b20d243ca9a951c6ba2c1
zyl <??>
2021-01-27 00:59:40 +0800

    first

---
+++ src/test/java/com/quartz/QuartzTest.java
@@ -0,0 +1,47 @@
+package com.quartz;// file: QuartzTest.java
+
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class QuartzTest {
+
+    public static void main(String[] args) {
+
+        try {
+            // Grab the Scheduler instance from the Factory
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+            // and start it off
+            scheduler.start();
+
+            JobDetail job = newJob(HelloJob.class)
+                    .withIdentity("job1", "group1")
+                    .build();
+
+            // Trigger the job to run now, and then repeat every 40 seconds
+            Trigger trigger = newTrigger()
+                    .withIdentity("trigger1", "group1")
+                    .startNow()
+                    .withSchedule(simpleSchedule()
+                            .withIntervalInSeconds(40)
+                            .repeatForever())
+                    .build();
+
+            // Tell quartz to schedule the job using our trigger
+            scheduler.scheduleJob(job, trigger);
+
+            Thread.sleep(60000);
+            scheduler.shutdown();
+
+        } catch (SchedulerException | InterruptedException se) {
+            se.printStackTrace();
+        }
+    }
+}
